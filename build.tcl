18ab18be26e4fd2ff9cca00eef0e7ffccd347732
Danilo Chang <ray2501@gmail.com>
2018-01-22 16:23:27 +0800

    Update build script

--- build.tcl
+++ build.tcl
@@ -2,9 +2,20 @@
 
 set arch "x86_64"
 set base "tclvfs-tclvfs"
-set fileurl "http://tclvfs.cvs.sourceforge.net/viewvc/tclvfs/tclvfs/?view=tar"
+set fileurl "https://core.tcl.tk/tclvfs/tarball/tclvfs-b5e463e712.tar.gz?r=b5e463e712ac1a876f443af284440b96fed622720caea2c2daf028a5b9f2f4a3"
 
-set var [list wget $fileurl -O $base.tar.gz]
+set var [list wget $fileurl -O tclvfs.tar.gz]
+exec >@stdout 2>@stderr {*}$var
+
+set var [list tar xzvf tclvfs.tar.gz]
+exec >@stdout 2>@stderr {*}$var
+
+file delete tclvfs.tar.gz
+
+set var [list mv tclvfs-b5e463e712 tclvfs-tclvfs]
+exec >@stdout 2>@stderr {*}$var
+
+set var [list tar czvf tclvfs-tclvfs.tar.gz tclvfs-tclvfs]
 exec >@stdout 2>@stderr {*}$var
 
 if {[file exists build]} {
@@ -19,5 +30,5 @@ set buildit [list rpmbuild --target $arch --define "_topdir [pwd]/build" -bb tcl
 exec >@stdout 2>@stderr {*}$buildit
 
 # Remove our source code
+file delete -force $base
 file delete $base.tar.gz
-
